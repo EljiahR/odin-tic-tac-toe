4bf2aa2900f978c7a1bbd37fb471031a0ab32d0d
Elijah <kcerhajile@gmail.com>
2023-09-17 21:20:40 -0400

    Modules for disaplyController and gameBoard added, factory function added for Players, board made in html

---
+++ javascript.js
@@ -0,0 +1,19 @@
+const gameBoard = (()=>{
+
+})();
+
+const displayController = (()=>{
+
+})();
+
+const Player =(name) =>{
+    const getName = () => name;
+
+    return {getName};
+};
+
+/*
+const eli = Player('Eli');
+const jaz = Player('Jaz');
+console.log(jaz.getName());
+*/
